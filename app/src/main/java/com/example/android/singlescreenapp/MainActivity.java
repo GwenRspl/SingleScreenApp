8ca2fd375d7d891331dc425773ced94b1c0818b5
Gwenaelle Rispal <gwenaelle.rispal@gmail.com>
2017-03-29 12:01:27 +0200

    [SingleScreenApp] added options to view location on map and to dial number

--- app/src/main/java/com/example/android/singlescreenapp/MainActivity.java
+++ app/src/main/java/com/example/android/singlescreenapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.singlescreenapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void goToMaps(View view) {
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=48.858370,2.294481"));
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+
+    protected void callNumber(View view) {
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:+33123456789"));
+        startActivity(callIntent);
     }
 }
